8140ca32522b85f16590319229ddc776509c7fa0
Daucko <dauda.a.tijani@gmail.com>
2024-05-13 20:58:05 +0100

    added favicon.ico and adjusted the background of the hero section

--- src/components/WelcomeSection/Welcome.jsx
+++ src/components/WelcomeSection/Welcome.jsx
@@ -4,10 +4,10 @@ import classes from './Welcome.module.css';
 const Welcome = () => {
   return (
     <main className={classes.main}>
-      <h1 className="text-5xl font-bold sm:ml-[50vw]">
+      <h1 className="text-5xl text-white sm:text-black font-bold sm:ml-[50vw]">
         WELCOME TO AniCare Hub
       </h1>
-      <p className="text-3xl sm:ml-[50vw] mt-5">
+      <p className="text-3xl text-white sm:text-black sm:ml-[50vw] mt-5">
         A platform for anything Animal
       </p>
     </main>
