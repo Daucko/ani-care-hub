9933cfc50921b7324a73acd222148d9c4146ef3c
Daucko <dauda.a.tijani@gmail.com>
2024-05-19 20:36:47 +0100

    Worked on the footer

--- src/components/layout/HomeFooter.jsx
+++ src/components/layout/HomeFooter.jsx
@@ -8,7 +8,9 @@ const HomeFooter = () => {
   return (
     <main className="border-t-1 border-[#2D3250]">
       <section className="flex flex-col sm:flex-row items-center justify-center sm:justify-between bg-[#7077A1] px-20 py-3">
-        <p className="text-white">&copy; Copyright. All rights reserved.</p>
+        <p className="text-white text-[12px] sm:text-[15px]">
+          &copy; Copyright. All rights reserved.
+        </p>
         <div className="flex justify-center items-center gap-x-5 mt-2 sm:mt-10">
           <Link className="bg-[#2D3250] p-3 rounded-full">
             <BsTwitterX className="h-5 w-5 text-white" />
