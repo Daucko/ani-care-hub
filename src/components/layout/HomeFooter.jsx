eee61a17050529dd15173bbcdfa857d4bf97e2a3
Daucko <dauda.a.tijani@gmail.com>
2024-05-20 20:57:23 +0100

    Worked on the footer

--- src/components/layout/HomeFooter.jsx
+++ src/components/layout/HomeFooter.jsx
@@ -11,17 +11,17 @@ const HomeFooter = () => {
         <p className="text-white text-[12px] sm:text-[15px]">
           &copy; Copyright. All rights reserved.
         </p>
-        <div className="flex justify-center items-center gap-x-5 mt-5 sm:mt-10">
-          <Link className="bg-[#2D3250] p-3 rounded-full">
+        <div className="flex justify-center gap-x-5 mt-5 sm:mt-0">
+          <Link className="bg-[#2D3250] p-2 rounded-full">
             <BsTwitterX className="h-5 w-5 text-white" />
           </Link>
-          <Link className="bg-[#2D3250] p-3 rounded-full">
+          <Link className="bg-[#2D3250] p-2 rounded-full">
             <FaFacebookF className="h-5 w-5 text-white" />
           </Link>
-          <Link className="bg-[#2D3250] p-3 rounded-full">
+          <Link className="bg-[#2D3250] p-2 rounded-full">
             <BsInstagram className="h-5 w-5 text-white" />
           </Link>
-          <Link className="bg-[#2D3250] p-3 rounded-full">
+          <Link className="bg-[#2D3250] p-2 rounded-full">
             <FaLinkedin className="h-5 w-5 text-white" />
           </Link>
         </div>
