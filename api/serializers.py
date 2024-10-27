81125685760ecf2879f96c499bc5292e0b111138
Daucko <dauda.a.tijani@gmail.com>
2024-10-27 13:18:27 +0000

    Rearranged class Meta in serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -4,14 +4,14 @@ from .models import Doctor
 class DoctorSerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Doctor
-        fields = [ "first_name", "last_name", "email", "password", "vcn_number", "address", "latitude", "longitude", "bio", 
-        "x_link", "facebook_link", "instagram_link", "linked_in_link", "image_url" ]
-
     def get_image_url(self, obj):
         request = self.context.get("request")
         image_url = obj.image.url
         return request.build_absolute_uri(image_url) if obj.image else None
+    
+    class Meta:
+        model = Doctor
+        fields = [ "first_name", "last_name", "email", "password", "vcn_number", "address", "latitude", "longitude", "bio", 
+        "x_link", "facebook_link", "instagram_link", "linked_in_link", "image_url" ]
 
     
